b013d0daa405173a68ad6cc8b528b58d6e15aa0a
Rizwan321 <125357675+RizwanSabir@users.noreply.github.com>
2024-02-08 11:59:20 -0800

    Update init.py

--- init.py
+++ init.py
@@ -1,35 +1,23 @@
-import yt_dlp
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from IPython.display import HTML
-from base64 import b64encode
-from moviepy.editor import VideoFileClip
+from pytube import YouTube
 
-# Replace the YouTube URL with your desired video URL
-video_url = "https://www.youtube.com/watch?v=NhmGNfILDyw"
+def download_youtube_video(url, output_path='/content/test.mp4'):
+    try:
+        # Create a YouTube object
+        yt = YouTube(url)
 
-!yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' {video_url}
+        # Get the highest resolution stream
+        video_stream = yt.streams.get_highest_resolution()
 
-video_path = 'downloaded_video.mp4'
+        # Download the video
+        print(f"Downloading: {yt.title}")
+        video_stream.download(output_path)
+        print("Download complete!")
 
-# In case if has downloaded it in Wbem
-# !ffmpeg -i /content/downloaded_video_mp4.webm  /content/downloadMP4.mp4
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-# Trim the video from seconds 108 to 120
-with VideoFileClip(video_path) as video:
-    trimmed_video = video.subclip(108, 120)
+# Example usage
+video_url = "https://www.youtube.com/watch?v=vkLQylA4388"
+output_path = "/content/test.mp4"
 
-    # Save the trimmed video
-    trimmed_video.write_videofile('trimmed_video.mp4')
-# Define the path to the trimmed video
-video_path = 'trimmed_video.mp4'
-
-# Create an HTML string to display the video
-video_html = HTML("""
-<video width=400 controls>
-  <source src="data:video/mp4;base64,{0}" type="video/mp4">
-</video>
-""".format(b64encode(open(video_path, "rb").read()).decode()))
-
-# Display the video
-display(video_html)
-
+download_youtube_video(video_url, output_path)
